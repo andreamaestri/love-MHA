0e3421ca1a189644d2ceed0b193116cc48858a35
Andrea Maestri <k4kbqw0j@students.codeinstitute.net>
2024-09-30 16:28:53 +0100

    UPDATE: Fix JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,72 @@
-// script.js
-
 gsap.registerPlugin(ScrollTrigger);
 
-// Parallax effect for the shape
+
 gsap.to(".shape", {
     yPercent: -50, // Move the shape up by 50% of its height
     ease: "power4.out", // Use a slower easing function
     scrollTrigger: {
         trigger: ".container-fluid",
-        start: "top top",
+        start: "top center",
         end: "bottom bottom",
         scrub: 0.05 // Adjust scrub for smoother scrolling
     }
 });
 
-// Morph to square in section 2
+
 gsap.to(".shape", {
     scrollTrigger: {
         trigger: ".section-2",
         start: "top center",
-        end: "bottom center",
+        end: "bottom bottom",
         scrub: true
     },
     borderRadius: "0%",
-    backgroundColor: "#fafafa"
+    backgroundColor: "rgba(244,244,244,0.37)"
 });
 
-// Morph back to circle in section 3
+
 gsap.to(".shape", {
     scrollTrigger: {
         trigger: ".section-3",
         start: "top center",
-        end: "bottom center",
+        end: "bottom bottom",
         scrub: true
     },
     borderRadius: "50%",
     backgroundColor: "#2391f9"
 });
 
-// Fade out shape in section 3
+
 gsap.to(".shape", {
     scrollTrigger: {
         trigger: ".section-3",
-        start: "top center",
+        start: "top top",
         end: "bottom top",
         scrub: true
     },
     opacity: 0
 });
 
-// Reveal text in each section with blur effect
+
 const sections = document.querySelectorAll("section");
 sections.forEach((section) => {
     gsap.to(section.querySelector(".text"), {
         scrollTrigger: {
             trigger: section,
             start: "top center",
-            end: "bottom center",
+            end: "bottom bottom",
             scrub: true
         },
         opacity: 1,
-        filter: "blur(0px)" // Remove blur as it scrolls into view
+        filter: "blur(0px)"
     });
 
-    // Reveal quote in each section
+
     gsap.to(section.querySelector(".quote"), {
         scrollTrigger: {
             trigger: section,
-            start: "top center",
-            end: "bottom center",
+            start: "top top",
+            end: "bottom bottom",
             scrub: true
         },
         opacity: 1
